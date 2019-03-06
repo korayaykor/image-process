8eae0befa901eb244871784ba3fa4c90ab0231d1
Koray <36260358+korayaykor@users.noreply.github.com>
2019-03-06 15:50:27 +0300

    Update cornerdetection.py
    explanation added.

--- cornerdetection.py
+++ cornerdetection.py
@@ -11,10 +11,10 @@ dst = cv2.cornerHarris(gray,2,3,0.1)
 
 dst = cv2.dilate(dst,None)
 
-# Threshold for an optimal value, it may vary depending on the image.
+# optimal value changes with photo.
 img[dst>0.01*dst.max()]=[0,0,255]
 cv2.namedWindow('dst', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('dst', 600,600)
-cv2.imshow('dst',img)#rewrite image
+cv2.imshow('dst',img)#rewrite image on screen
 if cv2.waitKey(0) & 0xff == 27:
     cv2.destroWindows()
